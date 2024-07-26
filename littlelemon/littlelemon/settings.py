fc66f08bac84faf8b5e4e073e6a52679d3f6dab6
adamKitoko <154778948+adamKitoko@users.noreply.github.com>
2024-07-25 20:47:38 -0400

    Creation des models

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -78,6 +78,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Bookings(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    bookingDate = models.DateTimeField()
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField()
+    inventory = models.IntegerField()
